1b9e139667e58ff386ee9f28c08cfa372e175f52
goryface <101987351+goryface@users.noreply.github.com>
2024-01-22 19:54:10 +0100

    Add files via upload

---
+++ Decentralization Technologies/GameofLife-Amine-3/main.js
@@ -0,0 +1,24 @@
+// Must be the same size as the game dimensions (10 x 10 in this case)
+var starting_cells = [
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
+  ];
+  
+  var params = {
+    canvas_id: "life",
+    cell_width: 10,
+    cell_height: 10,
+    init_cells: starting_cells
+  };
+  
+  // Create a new instance of the GameOfLife class with the specified parameters
+  var game = new GameOfLife(params);
+  
